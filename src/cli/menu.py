f4315d4f0d52e8bd6d9e63899a371a0155bed616
lambadajul <lambadajul@proton.me>
2026-07-29 18:16:48 +0300

    Add show_menu function to display CLI options

---
+++ src/cli/menu.py
@@ -0,0 +1,10 @@
+def show_menu():
+
+    print()
+
+    print("1. Subjects")
+    print("2. Schedule")
+    print("3. Homework")
+    print("4. Statistics")
+    print("5. Export")
+    print("6. Exit")
